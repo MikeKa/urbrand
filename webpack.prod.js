371febf92db6ad6c268504f164e5319787c2f1f2
mkashirsky <mkashirsky@gmail.com>
2017-10-09 08:57:34 -0400

    Flexview column

--- webpack.prod.js
+++ webpack.prod.js
@@ -56,6 +56,10 @@ module.exports = {
         loader: 'file-loader',
         options: { name: 'img/[name].[ext]' },
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
     ]
   },
   plugins: [
