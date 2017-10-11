348dc821e43abf081539ceb12e45e6434439e18c
mkashirsky <mkashirsky@gmail.com>
2017-10-11 10:32:19 -0400

    Mobile layout fix

--- src/frontend/js/components/pages/list/ListContainer.js
+++ src/frontend/js/components/pages/list/ListContainer.js
@@ -2,8 +2,7 @@ import { connect } from 'react-redux'
 
 import * as actionCreators from '../../../redux/action-creators'
 import { getShopItemsByCategory } from '../../../redux/reducers'
-//import List from './List'
-import List from '/List'
+import List from './List'
 
 const mapStateToProps = (state, ownProps) => {
   return {
